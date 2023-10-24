3ccfa5f2f0eb1fe31c8d9c39547dd5818988fe52
Maximo <maxmonteiro86@gmail.com>
2023-10-24 11:05:13 -0300

    cria layout do carrossel de slides e define array dos slides

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         <v-col cols="12" sm="12">
 
           <v-carousel>
-            <v-carousel-item v-for="(item, i) in items" :key="item" :src="item.src" cover>
+            <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
 
             </v-carousel-item>
           </v-carousel>
